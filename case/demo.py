0d9c27809d0345e5710df070e24f4a325e9379f0
yuanxu <yuanxu@bigo.sg>
2024-03-11 17:23:21 +0800

    add

--- case/demo.py
+++ case/demo.py
@@ -1,16 +1,19 @@
+import random
+
+
 def add(a, b):
     """
     @api {post} /add 两数相加
     @apiGroup 商业化
     @apiName add
     @apiDescription  两数求和，返回计算结果
-    @apiPermission fang
-    @apiParam {String=7777,9999} a=77777    数字类型
-    @apiParam {String=667777} b=9999   数字类型
+    @apiPermission test
+    @apiParam {String=111,222} a=111    数字类型
+    @apiParam {String=333} b=333   数字类型
     @apiParamExample {json} 请求示例:
     {
          "a": "111",
-         "b": "9999"
+         "b": "333"
       }
     @apiSuccess (200) {Number} code 服务器码
     @apiSuccess (200) {String} data 造数成功返回相关的数据
@@ -22,5 +25,37 @@ def add(a, b):
         "data": 12
     }
     """
-    sum = a+b
+    sum = a + b
     return dict(code=0, data=sum, msg="success")
+
+
+def randomNum(numList: list):
+    """
+    @api {post} /randomNum 随机数
+    @apiGroup 秩序
+    @apiName randomNum
+    @apiDescription 追加随机数
+    @apiParam {Object[]} numList 数据数组
+    @apiParam {String} numList.id 数据
+    @apiParam {String} numList.content 描述
+    @apiParamExample {json} 请求示例：
+    {
+        "numList": [
+            {"id":"123","content":"hello"},
+            {"id":"456","content":"world"}
+        ]
+    }
+    @apiSuccess (200) {Number} code 服务器码
+    @apiSuccess (200) {String} data 造数成功返回相关的数据
+    @apiSuccess (200) {String} msg 提示语
+    @apiSuccessExample {json} 返回示例:
+    {
+        "code": 0,
+        "msg": "请求成功",
+        "data": {}
+    }
+
+    """
+    for nums in numList:
+        nums['id'] = "{}-{}".format(nums['id'], random.randint(1, 10))
+    return dict(code=0, msg="success", data=numList)

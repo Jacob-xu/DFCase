918297d2970e7b9d7fa816665636cc7be5b1ba9a
yuanxu <yuanxu@bigo.sg>
2024-03-14 14:57:01 +0800

    add

--- case/demo.py
+++ case/demo.py
@@ -1,31 +1,35 @@
 import random
 
 
-def add(a, b):
+def add(name, email):
     """
-    @api {post} /add 两数相加
+    @api {post} /add 拼接字符串
     @apiGroup 商业化
     @apiName add
-    @apiDescription  两数求和，返回计算结果
-    @apiPermission test
-    @apiParam {String=111,222} a=111    第一个请求参数
-    @apiParam {String=333} b=333   第二个请求参数
+    @apiDescription  拼接字符串
+    @apiPermission 张三
+    @apiHeader product_id=123
+    @apiHeader task_id=123
+    @apiHeader task_type=123
+    @apiParam {String=zhangsan,lisi} name=zhangsan 用户名
+    @apiParam {String=222@bigo.com} email=222@bigo.sg 邮箱
     @apiParamExample {json} 请求示例:
     {
-         "a": "111",
-         "b": "333"
+         "name": "zhangsan",
+         "email": "222@bigo.sg"
       }
     @apiSuccess (200) {Number} code 服务器码
     @apiSuccess (200) {String} data 造数成功返回相关的数据
     @apiSuccess (200) {String} msg 提示语
     @apiSuccessExample {json} 返回示例:
     {
-        "code": 0,
+        "code": 200,
         "msg": "请求成功",
-        "data": 12
+        "data": zhangsan,222@bigo.sg
     }
     """
-    sum = a + b
+    sum = name + ',' + email
+    print(sum)
     return dict(code=0, data=sum, msg="success")
 
 
@@ -35,7 +39,7 @@ def randomNum(numList: list):
     @apiGroup 秩序
     @apiName randomNum
     @apiDescription 追加随机数
-    @apiPermission test2
+    @apiPermission 李四
     @apiParam {Object[]} numList 数据数组
     @apiParam {String} numList.id 这里填写id数据
     @apiParam {String} numList.content 这里填写描述
@@ -51,7 +55,7 @@ def randomNum(numList: list):
     @apiSuccess (200) {String} msg 提示语
     @apiSuccessExample {json} 返回示例:
     {
-        "code": 0,
+        "code": 200,
         "msg": "请求成功",
         "data": {}
     }
